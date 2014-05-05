6878ce9fd1f3e663106be35303d7e49926710102
Shelly Njoo <shelly@shellynjoo.com>
2014-05-05 17:55:47 -0400

    Email successfully sending, attachment not working yet

--- app/app/mailers/scheduled_mailer.rb
+++ app/app/mailers/scheduled_mailer.rb
@@ -1,8 +1,13 @@
 class ScheduledMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "mullenmailer@gmail.com"
   
-  def quintlysendmail
-
+  def sendcsvemail(csv)
+    attachments['quintly.csv'] = {mime_type: 'application/csv',
+                                  content: csv }
+    mail( to: 'snjoo@mullen.com', 
+          subject: 'Your scheduled Quintly CSV',
+          content_type: 'text/html',
+          body: 'testing!')
   end
   
 end
