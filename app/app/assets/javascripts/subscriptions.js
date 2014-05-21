7a0dfb69288fcc05871d8954c6c249fe1cdefa4f
Shelly Njoo <shelly@shellynjoo.com>
2014-05-21 17:54:26 -0400

    Quintly profile id hidden input field set up

--- app/app/assets/javascripts/subscriptions.js
+++ app/app/assets/javascripts/subscriptions.js
@@ -4,4 +4,46 @@ $(document).ready(function() {
     	$('#subscription_cron').val($(this).cron("value"));
     }
   }); 
+  
+  	// Adding Quintly Profile IDs from checkboxes  
+  	var collectQuintlyData = function(container, checkbox, input) {
+  	 // gather list of checkboxes
+  	  var checkboxes = $(container).find('input[type=checkbox]');
+	  $(checkbox).on('click', function() {
+	  	// start with empty array
+	  	var quintlyIDs =[];
+	  	checkboxes.each(function() {
+	  		if ($(this).prop('checked') == true ) {
+	  			quintlyIDs.push($(this).data('quintlyid'));
+	  		};
+	  	});
+	  	$(input).val(function() {
+	  		var str = '';
+	  		for (var i=0; i<quintlyIDs.length; i++) {
+	  			str = str+quintlyIDs[i]+',';
+	  		}
+	  		str = str.slice(0, - 1);
+	  		return str;
+	  	});
+	  	console.log(quintlyIDs);
+	  	// check for all checkboxes with prop checked == true
+	  	// push their datas to array
+	  	
+	  	
+	  	
+	  	
+	  	
+			// var checkbox = $(this);
+		  	// var id = checkbox.data('quintlyid');
+		  	// if (checkbox.prop('checked') == true) {
+		  		// quintlyIDs.push(id);
+		  		// console.log(quintlyIDs);
+		  	// } else {
+		  		// quintlyIDs.push(id);
+		  		// console.log(quintlyIDs);
+		  	// }
+		  // });
+		});
+	};
+	collectQuintlyData('#quintly_profileid_checkboxes','input.quintly_profileid', '#subscription_quintly_worker_attributes_quintly_profileids');
 });
